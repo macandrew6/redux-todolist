64498b9b460d07f5b07be72bec468c1de75645ed
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-13 19:59:07 -0800

    setting up index.html

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './root';
+
+
+ReactDOM.render(
+  <h1>Todo App</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./frontend/my_app.jsx",
+  entry: './frontend/entry.jsx',
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
   },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  },
+  }
 };
