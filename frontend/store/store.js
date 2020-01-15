7a101daedfbae338204ceeb0bc335e1bbcdc3e22
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-15 15:06:14 -0800

    writes configure store with preloadedstate arg

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,1 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => {
+  
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware()
+  );
+}
+
+
+export default configureStore;
