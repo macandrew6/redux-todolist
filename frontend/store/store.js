b4c73bbb7f6c3c42e0aca9ca39c40935e73ddb07
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-15 15:08:51 -0800

    adds logger from redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
 
 const configureStore = (preloadedState = {}) => {
-  
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware()
+    applyMiddleware(logger)
   );
-}
+};
 
 
 export default configureStore;
