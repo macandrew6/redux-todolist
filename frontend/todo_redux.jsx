7b5ccfc6e541d1d8b60860bddd41bb3af132b8cd
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-15 22:54:36 -0800

    built out allTodos selector

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
+
+import { 
+  receiveTodos, 
+  receiveTodo, 
+  RECEIVE_TODOS, 
+  RECEIVE_TODO } from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
   window.store = store; // be sure to remove this later
-
+  window.receiveTodo = RECEIVE_TODO;
+  window.receiveTodos = RECEIVE_TODOS;
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
+  
   ReactDOM.render(
-    <h1>a new thing</h1>,
+    <Root store={store} />,
     document.getElementById('content')
   );
 });
