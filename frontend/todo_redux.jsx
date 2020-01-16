69250da2f5635f95002ec35df03868c78b1d38c8
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-15 23:10:01 -0800

    issue with webpack

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { allTodos } from './reducers/selectors';
 
 import { 
   receiveTodos, 
@@ -13,10 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
   window.store = store; // be sure to remove this later
   window.receiveTodo = RECEIVE_TODO;
+  window.anything = 'anything';
   window.receiveTodos = RECEIVE_TODOS;
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
-  
+  window.apple = "apple";
+  window.allTodos = allTodos;
+
   ReactDOM.render(
     <Root store={store} />,
     document.getElementById('content')
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,55 @@
-var path = require('path');
+// var path = require('path');
+
+// module.exports = {
+//   entry: './frontend/todo_redux.jsx',
+//   output: {
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: [/\.jsx?$/],
+//         exclude: /(node_modules)/,
+//         use: {
+//           loader: 'babel-loader',
+//           query: {
+//             presets: ['@babel/env', '@babel/react']
+//           }
+//         },
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ['.js', '.jsx', '*']
+//   }
+// };
+
+const path = require("path");
 
 module.exports = {
-  entry: './frontend/todo_redux.jsx',
+  context: __dirname,
+  entry: "./frontend/todo_redux.jsx",
   output: {
-    filename: 'bundle.js',
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: [/\.jsx?$/],
+        test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           query: {
-            presets: ['@babel/env', '@babel/react']
+            presets: ["@babel/env", "@babel/react"]
           }
-        },
+        }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: [".js", ".jsx", "*"]
   }
 };
