fc21de5ddeb8d91742e40edd6593d6746bb6c7db
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-15 17:29:50 -0800

    testing redux store

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -4,9 +4,7 @@ import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
-  window.store = store;
-  let apple = 'apple';
-  window.apple = apple;
+  window.store = store; // be sure to remove this later
 
   ReactDOM.render(
     <h1>a new thing</h1>,
