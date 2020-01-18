9023b17d5eb5e50e095a3e3ab69f2027a12734ff
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-18 01:14:59 -0800

    requesting steps in tododetailview container

--- frontend/components/todo_list/TodoDetailView.jsx
+++ frontend/components/todo_list/TodoDetailView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import StepList from './StepList';
+
 
 const TodoDetailView = ({ todo, removeTodo }) => {
   return (
     <div>
       {todo.body}
-
+      <StepList />
     </div>
   );
 };
