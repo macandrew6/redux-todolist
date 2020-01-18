f53f10d4470714769035fce2ba17ae019d6fd302
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-18 00:50:54 -0800

    writes action creators for steps

--- frontend/actions/step_actions.js
+++ frontend/actions/step_actions.js
@@ -2,3 +2,17 @@ export const RECEIVESTEPS = 'RECEIVESTEPS';
 export const RECEIVESTEP = 'RECEIVESTEP';
 export const REMOVESTEP = 'REMOVESTEP';
 
+export const receiveSteps = steps => ({
+  type: RECEIVESTEPS,
+  steps
+});
+
+export const receiveStep = step => ({
+  type: RECEIVESTEP,
+  step
+});
+
+export const removeStep = step => ({
+  type: REMOVESTEP,
+  step
+});
