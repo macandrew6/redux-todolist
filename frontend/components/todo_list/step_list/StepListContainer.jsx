43330148fbabfc2941a79609b5c131dfa8765e03
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-18 14:55:55 -0800

    adds steps containers

---
+++ frontend/components/todo_list/step_list/StepListContainer.jsx
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import StepList from './StepList';
+
+const mapStateToProps = (state) => ({
+  
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StepList);
