deadde942e8f947b397eaf8261cad0c106a3e2bb
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-20 15:07:23 -0800

    makes passes removestep and receive step through steplistitem container

--- frontend/components/todo_list/step_list/StepListItemContainer.jsx
+++ frontend/components/todo_list/step_list/StepListItemContainer.jsx
@@ -2,12 +2,9 @@ import { connect } from 'react-redux';
 import StepListItem from './StepListItem';
 import { removeStep, receiveStep } from '../../../actions/step_actions';
 
-// const mapStateToProps = state => ({
-
-// });
-
 const mapDispatchToProps = dispatch => ({
-
+  removeStep: step => dispatch(removeStep(step)),
+  receiveStep: step => dispatch(receiveStep(step))
 });
 
 export default connect(
