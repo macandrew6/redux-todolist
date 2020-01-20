44d66ddb61627e791eabd4ec90959198dd0a2814
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-19 21:57:23 -0800

    pseudo codes steps

--- frontend/components/todo_list/step_list/StepForm.jsx
+++ frontend/components/todo_list/step_list/StepForm.jsx
@@ -4,7 +4,7 @@ class StepForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.state = { // place where step text must got and changed
 
     };
   }
