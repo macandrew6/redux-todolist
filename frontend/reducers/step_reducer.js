e33153398c25cf8997ead176c390a03181655dd8
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-22 14:06:02 -0800

    refactor receiveSteps reducer

--- frontend/reducers/step_reducer.js
+++ frontend/reducers/step_reducer.js
@@ -8,7 +8,11 @@ const stepsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_STEPS:
-      return Object.assign({}, state, action.steps);
+      return Object.assign(
+        {}, 
+        state, 
+        action.steps
+      );
     case RECEIVE_STEP:
       return Object.assign(
         {}, 
