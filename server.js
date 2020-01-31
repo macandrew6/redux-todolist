9736cfebe7e89f4330cca66c3a43981ffe212a35
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-30 21:31:36 -0800

    using node http

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-var express = require("express");
-const path = require("path");
-var app = express();
+// var express = require("express");
+// const path = require("path");
+// var app = express();
 
-app.set("port", process.env.PORT || 5000);
+// app.set("port", process.env.PORT || 5000);
 
-//For avoidong Heroku $PORT error
-app
-  .get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
+// //For avoidong Heroku $PORT error
+// app
+//   .get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "index.html"));
+//   })
+//   .listen(app.get("port"), function() {
+//     console.log(
+//       "App is running, server is listening on port ",
+//       app.get("port")
+//     );
+//   });
+
+var http = require("http");
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.send("it is running\n");
   })
-  .listen(app.get("port"), function() {
-    console.log(
-      "App is running, server is listening on port ",
-      app.get("port")
-    );
-  });
+  .listen(process.env.PORT || 5000);
