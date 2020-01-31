a56665ca8df98a3b45ce823d34c4281a2a9301ff
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-30 23:55:27 -0800

    reverted back to old webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/todo_redux.jsx",
+  entry: "./frontend/todo_redux.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js"
