47160245ff52341430cbe679e82838cebdbd9d8d
macandrew6 <mr.betterandrewmac@gmail.com>
2020-01-30 18:11:20 -0800

    adding express

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
+const path = require("path");
 var app = express();
 
 app.set("port", process.env.PORT || 5000);
 
 //For avoidong Heroku $PORT error
 app
-  .get("/", function(request, response) {
-    var result = "App is running";
-    response.send(result);
+  .get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "index.html"));
   })
   .listen(app.get("port"), function() {
     console.log(
